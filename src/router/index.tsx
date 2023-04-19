9387d9cffa4cb8411454993d9bed3c8f7955e0bb
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-04-19 20:12:24 +0900

    ⚡️:: 404페이지 및 라우터 연결

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AuthLoadingPage from "../pages/Auth/AuthLoadingPage";
+import LoginPage from "../pages/Auth/Login";
+import UserPage from "../pages/UserPage";
+import NotFound from "../components/Common/NotFound";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/callback" element={<AuthLoadingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/mypage" element={<UserPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
