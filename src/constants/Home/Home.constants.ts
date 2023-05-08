a1bc9452ef1ce8e2347ae064fa02236df2586e43
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-05-08 11:40:36 +0900

    ⚡️ :: 대대적인 변경

---
+++ src/constants/Home/Home.constants.ts
@@ -0,0 +1,18 @@
+interface Type {
+  categoryName: string;
+  categoryPath: string;
+}
+export const CATEGROY_ITEMS: Type[] = [
+  {
+    categoryName: "Home",
+    categoryPath: "/",
+  },
+  {
+    categoryName: "profile",
+    categoryPath: "/",
+  },
+  {
+    categoryName: "Review",
+    categoryPath: "/",
+  },
+];
