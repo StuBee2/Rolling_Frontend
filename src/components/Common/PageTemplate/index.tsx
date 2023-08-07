090b8dbba2513168b7544a7fc250ff05da797a7a
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-08-07 13:17:20 +0900

    ⚡️ :: webpack 적용

--- src/components/Common/PageTemplate/index.tsx
+++ src/components/Common/PageTemplate/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import GlobalStyle from "../../../styles/GlobalStyles";
 import Header from "../Header/index";
 import { useRecoilValue } from "recoil";
@@ -18,6 +18,11 @@ export default function PageTemplate({ children }: Props) {
   const hideHeader = useRecoilValue(HideHeader);
   const searchModal = useRecoilValue(SearchModal);
   const simpleInfoModal = useRecoilValue(SimpleInfoModal);
+  useEffect(() => {
+    window.onbeforeunload = () => {
+      window.scrollTo(0, 0);
+    };
+  }, []);
   return (
     <>
       <GlobalStyle />
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: path.resolve("./src/index.tsx"),
+  },
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    filename: "index.js",
+    publicPath: "/",
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg|png|jpg|gif$/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: ["dist"] }),
+  ],
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    extensions: [".js", ".ts", ".jsx", ".tsx"],
+  },
+};
