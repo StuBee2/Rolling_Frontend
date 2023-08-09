d10db7c3e53bf348836e3fdadff9057af8a4916f
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-08-09 21:41:28 +0900

    ✨ :: webpack-merge 추가, 🔥 yarn.lock 삭제

--- webpack.common.js
+++ webpack.common.js
@@ -4,20 +4,13 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: {
     main: path.resolve("./src/index.tsx"),
   },
-  devtool: "inline-source-map",
   output: {
     path: path.resolve(__dirname, "dist/"),
     filename: "index.js",
     publicPath: "/",
-  },
-  devServer: {
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
   },
   module: {
     rules: [
