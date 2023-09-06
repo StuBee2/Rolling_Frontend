9dbaa56b7fb6608b445b04a018042c5dcb938f66
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-09-06 17:22:10 +0900

    🔥 :: 더미데이터 삭제

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,7 +2,6 @@ import Category from "./Category";
 import CmsRank from "./CmsRank";
 import HomeList from "./HomeList";
 import * as S from "./style";
-import { Button } from "@stubee2/stubee2-rolling-ui";
 
 export default function Home() {
   return (
@@ -11,11 +10,6 @@ export default function Home() {
         <Category />
         <HomeList />
         <CmsRank />
-        <Button
-          children="리뷰등록"
-          ButtonType="review"
-          customStyle={{ color: "black" }}
-        />
       </S.HomeWrapper>
     </S.HomeContainer>
   );
