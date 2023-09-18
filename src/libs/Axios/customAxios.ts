005559319e257715b8f80246b8a621ffa0a0b490
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-09-18 22:02:53 +0900

    ⚡️ :: vercel 환경변수 생성

--- src/libs/Axios/customAxios.ts
+++ src/libs/Axios/customAxios.ts
@@ -8,10 +8,17 @@ import CONFIG from "@src/config/config.json";
 import { responseHandler } from "./responseHandler";
 
 export const customAxios = axios.create({
-  baseURL: `${CONFIG.SERVER}`,
-  headers: {
-    [REQUEST_TOKEN_KEY]: `Bearer ${Token.getToken(ACCESS_TOKEN_KEY)}`,
-  },
+  baseURL: process.env.API_KEY,
 });
+
+const accessToken = Token.getToken(ACCESS_TOKEN_KEY);
+
+if (accessToken) {
+  customAxios.defaults.headers.common[
+    REQUEST_TOKEN_KEY
+  ] = `Bearer ${accessToken}`;
+} else {
+  delete customAxios.defaults.headers.common[REQUEST_TOKEN_KEY];
+}
 
 customAxios.interceptors.response.use((response) => response, responseHandler);
