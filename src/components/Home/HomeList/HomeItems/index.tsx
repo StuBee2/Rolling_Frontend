e9a1f4410d32771fcb872084640dad12d2fd426d
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-09-20 17:29:19 +0900

    ⚡️ :: 글자길이 제한 추가

--- src/components/Home/HomeList/HomeItems/index.tsx
+++ src/components/Home/HomeList/HomeItems/index.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import { useRecoilValue } from "recoil";
 import * as S from "./style";
-import { getDateText } from "@stubee2/stubee2-rolling-util";
+import { getDateText, stringEllipsis } from "@stubee2/stubee2-rolling-util";
 
 export default function HomeItems() {
   const searchApiUrl = useRecoilValue(CompanyAllAndSearchAtom);
@@ -35,10 +35,13 @@ export default function HomeItems() {
                 </S.CompanyNameAndCreatedAt>
                 <div>
                   <S.CompanyDescription>
-                    {item.companyDetails.description}
+                    {stringEllipsis(item.companyDetails.description, 20)}
                   </S.CompanyDescription>
                   <S.CompanyAddress>
-                    {item.companyDetails.companyAddress.address}
+                    {stringEllipsis(
+                      item.companyDetails.companyAddress.address,
+                      28
+                    )}
                   </S.CompanyAddress>
                 </div>
                 <S.CompanyTagUl>
