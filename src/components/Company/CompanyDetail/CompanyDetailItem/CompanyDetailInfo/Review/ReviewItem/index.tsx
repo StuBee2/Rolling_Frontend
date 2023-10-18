0d52da118f5bc4e91a3b0239ef8b5a31b2cd2c51
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-10-18 13:46:20 +0900

    ⚡️ :: 회사 단일조회 리뷰item 컴포넌트 분리

---
+++ src/components/Company/CompanyDetail/CompanyDetailItem/CompanyDetailInfo/Review/ReviewItem/index.tsx
@@ -0,0 +1,60 @@
+import ReviewSetUp from "@src/components/Common/ReviewSetUp";
+import Star from "@src/components/Common/Star";
+import { ACCESS_TOKEN_KEY } from "@src/constants/Auth/auth.constant";
+import Token from "@src/libs/Token/Token";
+import { ReviewInfoIdType } from "@src/types/Review/review.type";
+import { jwtDecoding } from "@src/utils/Auth/jwtDecoding";
+import { changeReviewStarGradesToArrayObject } from "@src/utils/Review/changeReviewStarGradesToArrayObject";
+import * as S from "./style";
+
+export default function ReviewItem({ ...attr }: ReviewInfoIdType) {
+  const isCoincideMemberId = jwtDecoding("sub") === attr.writerId;
+  return (
+    <S.Container>
+      <S.Wrapper>
+        <S.ContentContainer>
+          <S.UserInfo>
+            <S.Profile>
+              <S.ImgUrl src={attr.memberImageUrl} alt="이미지 없음" />
+              <S.NickName>
+                {attr.memberNickName || attr.memberSocialId}
+              </S.NickName>
+            </S.Profile>
+
+            <S.PositionAndCareerPath>
+              <p>
+                <span>포지션 · </span>
+                {attr.reviewPosition}
+              </p>
+              <p>
+                <span>입사경로 · </span>
+                {attr.reviewCareerPath}
+              </p>
+            </S.PositionAndCareerPath>
+          </S.UserInfo>
+
+          <S.ContentWrapper>
+            <S.Description>{attr.reviewContent}</S.Description>
+            {Token.getToken(ACCESS_TOKEN_KEY) && isCoincideMemberId && (
+              <S.SetUp>
+                <ReviewSetUp
+                  reviewId={attr.reviewId}
+                  companyId={attr.companyId!!}
+                />
+              </S.SetUp>
+            )}
+          </S.ContentWrapper>
+        </S.ContentContainer>
+
+        <S.StarRatingContainer>
+          <Star
+            rankStatus={changeReviewStarGradesToArrayObject(attr)}
+            width={20}
+            height={20}
+            fontSize={"15px"}
+          />
+        </S.StarRatingContainer>
+      </S.Wrapper>
+    </S.Container>
+  );
+}
