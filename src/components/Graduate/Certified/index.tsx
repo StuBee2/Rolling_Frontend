d3eed6b713794338ac837089811fa2c67d82a30e
전해림 <HAERIM060920@gmail.com>
2023-10-19 21:56:02 +0900

    ⚡ :: 졸업생 인증 반응형

--- src/components/Graduate/Certified/index.tsx
+++ src/components/Graduate/Certified/index.tsx
@@ -7,32 +7,34 @@ const Certification = () => {
   const { handleQuestionChange, handleGraduateCertified } = useCertify();
   return (
     <S.CertificationContainer>
-      <S.BackGroundImg src={backgroundimg} alt="이미지 없음" />
-      <div style={{ zIndex: "1" }}>
+      <S.TTest>
         <S.CertificationBox>
           <S.CertificationList>
-            <S.Title className="bold-text">DGSW 졸업생 인증</S.Title>
-            <S.CertificationInfo>
-              해당 절차는 DGSW졸업생을 인증하여 서비스를 제공하기 위해
-              이루어집니다.
-              <br />
-              졸업생 인증으로 롤링의 더 많은 기능을 활성화하세요.
-            </S.CertificationInfo>
-            <S.QuestionContainer onSubmit={(e) => handleGraduateCertified(e)}>
-              <S.QuestionList>
-                <S.QuestionText className="bold-text">
-                  Q. DGSW 사감선생님 중, 한 분의 성함을 입력해주세요.
-                </S.QuestionText>
-                <S.InputContainer>
-                  <S.Input
-                    onChange={handleQuestionChange}
-                    id="housemaster"
-                    name="housemaster"
-                    placeholder="선생님의 성함을 적어주세요"
-                  />
-                  <S.Teacher>선생님</S.Teacher>
-                </S.InputContainer>
+            <S.Test>
+              <S.Title className="bold-text">DGSW 졸업생 인증</S.Title>
+              <S.CertificationInfo>
+                <p>
+                  해당 절차는 DGSW졸업생을 인증하여 서비스를 제공하기 위해
+                  이루어집니다.
+                </p>
 
+                <p> 졸업생 인증으로 롤링의 더 많은 기능을 활성화하세요.</p>
+              </S.CertificationInfo>
+              <S.QuestionContainer onSubmit={(e) => handleGraduateCertified(e)}>
+                <S.QuestionList>
+                  <S.QuestionText className="bold-text">
+                    Q. DGSW 사감선생님 중, 한 분의 성함을 입력해주세요.
+                  </S.QuestionText>
+                  <S.InputContainer>
+                    <S.Input
+                      onChange={handleQuestionChange}
+                      id="housemaster"
+                      name="housemaster"
+                    />
+
+                    <S.Teacher>선생님</S.Teacher>
+                  </S.InputContainer>
+                </S.QuestionList>
                 <Button
                   type="submit"
                   ButtonType="custom"
@@ -40,11 +42,11 @@ const Certification = () => {
                 >
                   인증하기
                 </Button>
-              </S.QuestionList>
-            </S.QuestionContainer>
+              </S.QuestionContainer>
+            </S.Test>
           </S.CertificationList>
         </S.CertificationBox>
-      </div>
+      </S.TTest>
     </S.CertificationContainer>
   );
 };
--- src/components/Graduate/Certified/style.ts
+++ src/components/Graduate/Certified/style.ts
@@ -1,28 +1,46 @@
 import styled from "styled-components";
 
-export const BackGroundImg = styled.img`
-  height: 100%;
-
-  background-size: cover;
-
-  position: fixed;
-  top: 0;
-  left: 0;
-`;
-
 export const CertificationContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 
+  width: 100%;
   height: 100vh;
 
+  min-width: 500px;
+  background-color: rgba(222, 224, 230, 1);
+
+  object-fit: fill;
+
   padding-top: 90px;
+
+  @media screen and (max-width: 498px) {
+    background: none;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+
+    margin-top: 50px;
+  }
+`;
+
+export const TTest = styled.div``;
+
+export const Test = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 90%;
 `;
 
 export const Input = styled.input`
-  width: 403px;
-  height: 80px;
+  width: 50%;
+  height: 60px;
 
   border: 0;
   background-color: #bdc2d0;
@@ -41,22 +59,30 @@ export const CertificationBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 `;
 
 export const Title = styled.div`
   margin-bottom: 38px;
 
-  font-size: 40px;
+  font-size: 30px;
 `;
 
 export const CertificationInfo = styled.div`
   display: flex;
+  flex-direction: column;
   text-align: center;
 
-  font-size: 18px;
+  font-size: 16px;
   color: #61677e;
 
   margin-bottom: 53px;
+
+  row-gap: 0.8rem;
+
+  @media screen and (max-width: 960px) {
+    font-size: 12px;
+  }
 `;
 
 export const QuestionContainer = styled.form`
@@ -64,8 +90,8 @@ export const QuestionContainer = styled.form`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 1059px;
-  height: 387px;
+
+  width: 100%;
 
   background-color: #dee0e6;
 
@@ -73,13 +99,17 @@ export const QuestionContainer = styled.form`
 `;
 
 export const QuestionText = styled.div`
-  font-size: 24px;
+  font-size: 22px;
 
-  margin-bottom: 49px;
+  margin-bottom: 40px;
+
+  @media screen and (max-width: 960px) {
+    font-size: 17px;
+  }
 `;
 
 export const Button = {
-  width: "1059px",
+  width: "100%",
   height: "68px",
 
   backgroundColor: "#4869f6",
@@ -98,9 +128,14 @@ export const Button = {
 
 export const InputContainer = styled.div`
   display: flex;
+  justify-content: center;
+
   align-items: flex-end;
+  text-align: center;
 
   margin-bottom: 76px;
+
+  width: 100%;
 `;
 
 export const CertificationList = styled.div`
@@ -111,9 +146,23 @@ export const CertificationList = styled.div`
 
   border-radius: 20px;
 
-  width: 1200px;
+  width: 1220px;
   height: 701px;
+
   background-color: white;
+
+  @media screen and (max-width: 1250px) {
+    width: 180%;
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 125%;
+  }
+  @media screen and (max-width: 498px) {
+    width: 95%;
+
+    border: 1px solid black;
+  }
 `;
 
 export const QuestionList = styled.div`
@@ -123,11 +172,10 @@ export const QuestionList = styled.div`
   align-items: center;
 
   margin-top: 89px;
+
+  width: 90%;
 `;
 
 export const Teacher = styled.div`
-  display: flex;
-
   font-size: 23px;
-  justify-content: flex-end;
 `;
