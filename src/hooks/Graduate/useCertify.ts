2691c061b134bbe01b17eaece150a70a154564ec
전해림 <HAERIM060920@gmail.com>
2023-10-20 14:05:10 +0900

    ⚡ :: 동문인증 토큰 재발급

--- src/hooks/Graduate/useCertify.ts
+++ src/hooks/Graduate/useCertify.ts
@@ -1,7 +1,14 @@
+// import refresh
 import { usePostCertifyMutation } from "@src/queries/Graduate/graduate.query";
+import authRepositoryImpl from "@src/repositories/Auth/auth.repositoryImpl";
 import { GraduateCertifiedType } from "@src/types/Graduate/graduate.type";
 import { useRollingToast } from "@stubee2/stubee2-rolling-toastify";
-import { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import Token from "@src/libs/Token/Token";
+import {
+  ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_KEY,
+} from "@src/constants/Auth/auth.constant";
 
 export const useCertify = () => {
   const [certifiedData, setCertifiedData] = useState<GraduateCertifiedType>({
@@ -15,6 +22,22 @@ export const useCertify = () => {
   const handleQuestionChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setCertifiedData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const TokenRefresh = async () => {
+    const refresh_token = Token.getToken(REFRESH_TOKEN_KEY);
+    if (refresh_token) {
+      try {
+        const { accessToken: newAccessToken } =
+          await authRepositoryImpl.postRefreshToken({
+            refreshToken: refresh_token,
+          });
+
+        Token.setToken(ACCESS_TOKEN_KEY, newAccessToken);
+      } catch (e) {
+        console.log(e);
+      }
+    }
   };
 
   const handleGraduateCertified = (e: React.FormEvent<HTMLFormElement>) => {
@@ -31,6 +54,7 @@ export const useCertify = () => {
       {
         onSuccess: () => {
           rollingToast("졸업생 인증 성공", "success");
+          TokenRefresh();
         },
         onError: () => {
           rollingToast("졸업생 인증 실패", "error");
