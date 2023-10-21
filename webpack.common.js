e817044e830af9f3974e3d0c84ac961c6899f30c
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-10-21 17:18:58 +0900

    ⚡️ :: favicon 적용

--- webpack.common.js
+++ webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpg|gif|mp4|jpeg)$/,
+        test: /\.(png|jpg|gif|mp4|jpeg|ico)$/,
         type: "asset",
       },
       {
@@ -60,6 +60,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "index.html",
+      favicon: "./public/logo.ico",
     }),
     new RefreshWebpackPlugin(),
     new webpack.DefinePlugin({
