e367d2c9ac8d9deee98f1e15f4279157cb865c13
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-10-25 18:11:35 +0900

    ⚡️ ::경로 수정

--- src/hooks/Auth/useSocialLogin.ts
+++ src/hooks/Auth/useSocialLogin.ts
@@ -16,8 +16,8 @@ export function useSocialLogin() {
     if (accessToken && refreshToken) {
       Token.setToken(ACCESS_TOKEN_KEY, accessToken.toString());
       Token.setToken(REFRESH_TOKEN_KEY, refreshToken.toString());
-      navigate("alumni/certify");
-      window.location.replace("alumni/certify");
+      navigate("/");
+      window.location.replace("/");
     }
   }, [accessToken, refreshToken, navigate]);
 }
